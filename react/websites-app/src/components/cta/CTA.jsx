eacffd0fb3ac69f1fb37b53d7ce174cd837454a5
lirui <rayestonal@gmail.com>
2024-07-08 23:18:45 +0800

    react: complete for  a website app

--- react/websites-app/src/components/cta/CTA.jsx
+++ react/websites-app/src/components/cta/CTA.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './cta.css'
+import React from "react";
+import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>pppp</p>
+        <h3>hhhhh</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
