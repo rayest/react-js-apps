b94c9744c3e13f133dd1f4d0d9055bb8f11a908e
lirui <rayestonal@gmail.com>
2024-07-08 16:01:53 +0800

    react: a set up and nav bar for  a website app

---
+++ react/websites-app/README.md
@@ -0,0 +1,5 @@
+> xxx.jsx --> index.js -> App.js
+
+1. create components
+2. create xxx.jsx and xx.css in folder components
+3. import xxx.jsx in App.js
---
+++ react/websites-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../logo.svg";
+import "./navbar.css";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      {/* part links  */}
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+
+      {/* part sign in */}
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+
+      {/* 这段代码表示点击菜单按钮时，显示菜单，点击关闭按钮时，关闭菜单 */}
+      <div className="gpt3__navbar-menu">
+        {/* toggleMenu 初始的值是 false，表示标签是菜单按钮 */}
+        {toggleMenu ? (
+          <RiCloseLine /* 这个标签是关闭按钮 */
+            color="#fff"
+            size={27}
+            onClick={() => {
+              setToggleMenu(false);
+            }}
+          />
+        ) : (
+          <RiMenu3Line /* 这个标签是默认的初始的菜单按钮 */
+            color="#fff"
+            size={27}
+            onClick={() => {
+              setToggleMenu(true);
+            }}
+          />
+        )}
+        {/** 如果 toggleMenu 为 true，显示菜单 */}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#wgpt3">What is GPT3?</a>
+              </p>
+              <p>
+                <a href="#possibility">Open AI</a>
+              </p>
+              <p>
+                <a href="#features">Case Studies</a>
+              </p>
+              <p>
+                <a href="#blog">Library</a>
+              </p>
+            </div>
+            <div className="gpt3__navbar-menu_container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
